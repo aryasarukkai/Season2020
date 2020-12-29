5cdcf01c15acc545480dca2fdec0f15fa7aa67cf
Arya Sarukkai <66439562+aryasarukkai@users.noreply.github.com>
2020-12-28 17:12:59 -0800

    Update trip1.py

--- trip1.py
+++ trip1.py
@@ -6,17 +6,24 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
+from time import sleep
+# import line_follow.py
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 
+# Start a stopwatch to measure elapsed time
+watch = StopWatch()
+
 # Create your objects here.
-ev3dev = EV3Brick()
+ev3 = EV3Brick()
 
 # Initilize our motors
 left_motor = Motor(Port.A)
 right_motor = Motor(Port.D)
 front_motor_1 = Motor(Port.C)
 
+# Initialize the color sensor.
+right_sensor = ColorSensor(Port.S4)
 
 ARM_MOTOR_SPEED = 400
 WHEEL_DIAMETER = 92
@@ -25,13 +32,12 @@ DRIVE_SPEED_NORMAL = 200
 DRIVE_SPEED_SLOW = 100
 DRIVE_EXTRA_SLOW = 30
 CIRCUMFERENCE = 3.14 * WHEEL_DIAMETER # Diameter = 100mm, Circumference = 314.10mm = 1 rotation
-ROTATION = 3.14 * WHEEL_DIAMETER # Diameter = 92mm, Circumference = mm = 1 rotation
 
 # All parameters are in millimeters
 robot = DriveBase(left_motor, right_motor, wheel_diameter=WHEEL_DIAMETER, axle_track=AXLE_TRACK)
 
 # Set the straight speed to 100 and turn speed to 100
-robot.settings(straight_speed=DRIVE_SPEED_NORMAL, turn_rate=30)
+robot.settings(straight_speed=DRIVE_SPEED_NORMAL, turn_rate=30)   
 
 # Move forward to Step Counter
 robot.straight(700)
@@ -43,12 +49,13 @@ robot.settings(straight_speed=DRIVE_EXTRA_SLOW, turn_rate=30)
 i=0
 for i in range(0,28):
     robot.straight(10)
+    sleep(0.4)
     i = i + 1
 
 robot.stop()
 robot.settings(straight_speed=20, turn_rate=20)
-
-robot.turn(-20)
+robot.straight(-50)
+robot.turn(-90)
 '''
 steering_drive.on_for_seconds(0,SpeedPercent(1.5), 17.5)
 
