696c5fe239782f1b0de18c8e3232ee24aab4702e
Arya Sarukkai <66439562+aryasarukkai@users.noreply.github.com>
2021-01-04 17:07:14 -0800

    commit

--- all_trips.py
+++ all_trips.py
@@ -6,6 +6,7 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
+from time import sleep
 
 import config
 import drive_utils
@@ -64,92 +65,95 @@ watch = StopWatch()
 ev3 = EV3Brick()
 
 # Select or skip this trip
-ev3.speaker.say("TRIP ONE")
+ev3.speaker.say("To Run trip 1 press the LEFT BUTTON")
 ev3.screen.print("TRIP 1")
 
-if Button.LEFT in ev3.buttons.pressed(): 
-    # Initilize our motors
-    left_motor = Motor(Port.A)
-    right_motor = Motor(Port.D)
-    front_motor_1 = Motor(Port.C)
+i=0
+while Button.LEFT not in ev3.buttons.pressed():
+    i = i + 1
 
-    # Initialize the color sensors and motors.
-    right_sensor = ColorSensor(Port.S1)
-    left_sensor = ColorSensor(Port.S4)
-    ARM_MOTOR_SPEED = 400
-    WHEEL_DIAMETER = 92
-    AXLE_TRACK = 130
-    DRIVE_SPEED_FAST = 350
-    DRIVE_SPEED_NORMAL = 200
-    DRIVE_SPEED_SLOW = 100
-    DRIVE_EXTRA_SLOW = 30
-    CIRCUMFERENCE = 3.14 * WHEEL_DIAMETER # Diameter = 100mm, Circumference = 314.10mm = 1 rotation
+# Initilize our motors
+left_motor = Motor(Port.A)
+right_motor = Motor(Port.D)
+front_motor_1 = Motor(Port.C)
 
-    # All parameters are in millimeters
-    robot = DriveBase(left_motor, right_motor, wheel_diameter=WHEEL_DIAMETER, axle_track=AXLE_TRACK)
+# Initialize the color sensors and motors.
+right_sensor = ColorSensor(Port.S1)
+left_sensor = ColorSensor(Port.S4)
+ARM_MOTOR_SPEED = 400
+WHEEL_DIAMETER = 92
+AXLE_TRACK = 130
+DRIVE_SPEED_FAST = 350
+DRIVE_SPEED_NORMAL = 200
+DRIVE_SPEED_SLOW = 100
+DRIVE_EXTRA_SLOW = 30
+CIRCUMFERENCE = 3.14 * WHEEL_DIAMETER # Diameter = 100mm, Circumference = 314.10mm = 1 rotation
 
-    # Move forward to Step Counter
-    robot.settings(straight_speed=DRIVE_SPEED_FAST, turn_rate=30)   
-    robot.straight(800)
-    # Slow down and start slowly pushing forward
-    robot.stop()
-    robot.settings(straight_speed=DRIVE_EXTRA_SLOW, turn_rate=30)
+# All parameters are in millimeters
+robot = DriveBase(left_motor, right_motor, wheel_diameter=WHEEL_DIAMETER, axle_track=AXLE_TRACK)
 
-    # Do Step Counter (Small Steps so it doesn't get stuck)
-    i=0
-    for i in range(0,20):
-        robot.straight(10)
-        sleep(0.4)
-        i = i + 1
-    robot.stop()
+# Move forward to Step Counter
+robot.settings(straight_speed=DRIVE_SPEED_FAST, turn_rate=30)   
+robot.straight(800)
+# Slow down and start slowly pushing forward
+robot.stop()
+robot.settings(straight_speed=DRIVE_EXTRA_SLOW, turn_rate=30)
 
-    # Backup a little
-    robot.straight(-50)
-    robot.stop()
-
-    # Rotate left to align with wall
-    robot.settings(straight_speed=100, turn_rate=30)
-    robot.turn(-135)
-    robot.stop()
-
-    # Back up to wall
-    robot.settings(straight_speed=DRIVE_SPEED_SLOW, turn_rate=30)
-    robot.straight(-45)
-
-    # Go straight a little bit
-    robot.straight(138)
-    robot.stop()
-
-    # Rotate Right
-    robot.settings(straight_speed=DRIVE_SPEED_SLOW, turn_rate=30)
-    robot.turn(90)
-    robot.stop()
-
-    # Go straight to get closer to the treadmill
-    robot.settings(straight_speed=DRIVE_SPEED_NORMAL, turn_rate=30)
-    robot.straight(560)
-    robot.stop()
-
-    # Look for black followed by white to position the tire exactly on the treadmill
-    robot.settings(straight_speed=50, turn_rate=30)
-    drive_utils.drive_till_white(robot, left_sensor)
-
-    # Tweaking to get to exact position HCK1
+# Do Step Counter (Small Steps so it doesn't get stuck)
+i=0
+for i in range(0,20):
     robot.straight(10)
-    robot.stop()
+    sleep(0.4)
+    i = i + 1
+robot.stop()
 
-    # Rotate the tire to move the tradmill
-    # front_motor_1.run_time(speed=config.ARM_MOTOR_SPEED, time=500, then=Stop.HOLD, wait=False)
-    front_motor_1.run_angle(5* config.ARM_MOTOR_SPEED, -5000, then=Stop.HOLD, wait=True)
+# Backup a little
+robot.straight(-50)
+robot.stop()
 
-    # Come back home
-    robot.settings(straight_speed=DRIVE_SPEED_FAST, turn_rate=30)
-    robot.straight(-1880)
+# Rotate left to align with wall
+robot.settings(straight_speed=100, turn_rate=30)
+robot.turn(-135)
+robot.stop()
 
-    # End of trip: Stop Robot and all motors
-    robot.stop()
-    front_motor_1.stop()
-    front_motor_2.stop()
+# Back up to wall
+robot.settings(straight_speed=DRIVE_SPEED_SLOW, turn_rate=30)
+robot.straight(-45)
+
+# Go straight a little bit
+robot.straight(138)
+robot.stop()
+
+# Rotate Right
+robot.settings(straight_speed=DRIVE_SPEED_SLOW, turn_rate=30)
+robot.turn(90)
+robot.stop()
+
+# Go straight to get closer to the treadmill
+robot.settings(straight_speed=DRIVE_SPEED_NORMAL, turn_rate=30)
+robot.straight(560)
+robot.stop()
+
+# Look for black followed by white to position the tire exactly on the treadmill
+robot.settings(straight_speed=50, turn_rate=30)
+drive_utils.drive_till_white(robot, left_sensor)
+
+# Tweaking to get to exact position HCK1
+robot.straight(10)
+robot.stop()
+
+# Rotate the tire to move the tradmill
+# front_motor_1.run_time(speed=config.ARM_MOTOR_SPEED, time=500, then=Stop.HOLD, wait=False)
+front_motor_1.run_angle(5* config.ARM_MOTOR_SPEED, -5000, then=Stop.HOLD, wait=True)
+
+# Come back home
+robot.settings(straight_speed=DRIVE_SPEED_FAST, turn_rate=30)
+robot.straight(-1880)
+
+# End of trip: Stop Robot and all motors
+robot.stop()
+front_motor_1.stop()
+front_motor_2.stop()
 
 # TRIP 2 CODE ########################
 
